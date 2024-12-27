7b918ed844453b177f9ac173a84d6fdccb579903
pibence <pipis.bence@gmail.com>
2024-12-27 10:32:08 +0100

    add untracted changes

---
+++ src/input_prompts.py
@@ -0,0 +1,36 @@
+PROMPTS = {
+    "en": {
+        "welcome": "Welcome! Please select a language.",
+        "select_language": "Select a language (en/hu): ",
+        "enter_first_name": "Please enter the first name of the person you are looking for: ",
+        "enter_last_name": "Please enter the last name of the person you are looking for: ",
+        "enter_person_id": (
+            "Please enter the ID of the person you are looking for! "
+            "The IDs are in the table above. Copy and paste the ID here: "
+        ),
+        "invalid_id": "Please enter a valid ID.",
+        "plotting_tree": "Plotting the family tree for ID {person_id}...",
+        "continue": "Would you like to continue with a new person (yes/no)? ",
+        "yes_or_no": "Please enter 'yes' or 'no'.",
+        "invalid_name": "Please enter a valid name!",
+        "yes": "yes",
+        "no": "no",
+    },
+    "hu": {
+        "welcome": "Üdvözöljük! Kérjük, válasszon nyelvet.",
+        "select_language": "Válasszon nyelvet (en/hu): ",
+        "enter_first_name": "Kérjük, adja meg a keresett személy keresztnevét: ",
+        "enter_last_name": "Kérjük, adja meg a keresett személy vezetéknevét: ",
+        "enter_person_id": (
+            "Kérjük, adja meg a keresett személy azonosítóját! "
+            "Az azonosítók a fenti táblázatban találhatók. Másolja ki és illessze be az azonosítót: "
+        ),
+        "invalid_id": "Kérjük, adjon meg érvényes azonosítót.",
+        "plotting_tree": "Az ID {person_id} családfájának ábrázolása...",
+        "continue": "Szeretné folytatni egy új személlyel (igen/nem)? ",
+        "yes_or_no": "Kérjük, írja be az 'igen' vagy 'nem' szót.",
+        "invalid_name": "Kérjük, adjon meg érvényes nevet!",
+        "yes": "igen",
+        "no": "nem",
+    },
+}
